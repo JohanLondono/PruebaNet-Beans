70faff0107598d6ea3776083ed87084e276a4ba2
JohanLondono <johann.londonos@uqvirtual.edu.co>
2023-09-21 22:37:10 -0500

    Inicializando el Repositorio

---
+++ src/pruebanet/beans/PruebaNetBeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebanet.beans;
+
+/**
+ *
+ * @author johan
+ */
+public class PruebaNetBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
